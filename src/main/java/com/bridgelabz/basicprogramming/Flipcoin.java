29fca8a11ae232ee03be3694350a7fd8626cc51d
yaminijyothi <yamini0234@gmail.com>
2020-01-06 18:09:36 +0530

    [yaminijyothi] add. Flipcoin programm added

---
+++ src/main/java/com/bridgelabz/basicprogramming/Flipcoin.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.basicprogramming;
+
+import com.bridgelabz.utility.Utility;
+
+public class Flipcoin {
+	public static void main(String[] args)
+	{
+		System.out.println("enter a number to flip a coin:");
+		int n=Utility.getint();
+		int heads=0;
+		int tails=0;
+		int count=1;
+		double randNum=Math.random();
+		while(count<randnum) {
+		if(Math.random()<0.5)
+		{
+			tails++;
+		}
+		else
+		{
+			heads++;
+		}
+		System.out.println("tails="+tails);
+		System.out.println("heads="+heads);
+	}
+}
