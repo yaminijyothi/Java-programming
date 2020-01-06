a03091cfb6b4edf6ef2123189103b93bbb8c23d3
yaminijyothi <yamini0234@gmail.com>
2020-01-06 18:51:21 +0530

    [yaminijyothi] refractor. modified Flipcoin program

--- src/main/java/com/bridgelabz/basicprogramming/Flipcoin.java
+++ src/main/java/com/bridgelabz/basicprogramming/Flipcoin.java
@@ -5,22 +5,26 @@ import com.bridgelabz.utility.Utility;
 public class Flipcoin {
 	public static void main(String[] args)
 	{
-		System.out.println("enter a   number to flip a coin:");
-		int n=Utility.getint();
 		int heads=0;
 		int tails=0;
 		int count=1;
-		double randNum=Math.random();
-		while(count<randnum) {
-		if(Math.random()<0.5)
-		{
-			tails++;
+		System.out.println("enter a number to flip a coin:");
+		int n=Utility.getint();
+		while(count<=n) {
+			double randNum=Math.random();
+			if(randNum<0.5)
+			{
+				tails++;
+				System.out.println("tails="+tails);
+			}
+			else
+			{
+				heads++;
+				System.out.println("heads="+heads);
+			}
+			count++;
 		}
-		else
-		{
-			heads++;
-		}
-		System.out.println("tails="+tails);
-		System.out.println("heads="+heads);
+		System.out.println("percentage of tails="+(tails/10)*100);
+		System.out.println("percentage of heads="+(heads/10)*100);
 	}
 }
