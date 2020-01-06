85ebe672d24a91aa46b162ca93bfc82e132eea42
yaminijyothi <yamini0234@gmail.com>
2020-01-06 18:19:56 +0530

    [yaminijyothi] refactor. modified flipcoin program

--- src/main/java/com/bridgelabz/basicprogramming/Flipcoin.java
+++ src/main/java/com/bridgelabz/basicprogramming/Flipcoin.java
@@ -5,7 +5,7 @@ import com.bridgelabz.utility.Utility;
 public class Flipcoin {
 	public static void main(String[] args)
 	{
-		System.out.println("enter a number to flip a coin:");
+		System.out.println("enter a   number to flip a coin:");
 		int n=Utility.getint();
 		int heads=0;
 		int tails=0;
