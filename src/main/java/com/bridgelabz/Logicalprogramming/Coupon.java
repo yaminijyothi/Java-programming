3c28dd4c1606e381e15b72bd1cdee6c2ae28027e
yaminijyothi <yamini0234@gmail.com>
2020-01-07 15:42:06 +0530

    [yaminijyothi] add. coupon numbers programm added

---
+++ src/main/java/com/bridgelabz/Logicalprogramming/Coupon.java
@@ -0,0 +1,32 @@
+/* 
+ * @purpose : Demonstrate the elapsed time in stopwatch
+ * @ file   : stopwatch.java
+ * @ author : yaminijyothi
+ */
+package com.bridgelabz.Logicalprogramming;
+
+import com.bridgelabz.utility.Utility;
+import java.util.Random;
+
+public class Coupon {
+//main
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter number of coupons requied:");
+		int n = Utility.getint();
+		int arr[] = new int[10];
+		Random rand = new Random();
+		//logic for non repeated coupon numbers
+		for (int i = 0; i < n; i++) {
+			int coupon = rand.nextInt(1000);
+			if (coupon != arr[i])
+
+			{
+				System.out.println(coupon + "");
+
+			}
+
+		}
+	}
+
+}
