9be6069351a08d4fdc1540210fec28e1d90a9982
yaminijyothi <yamini0234@gmail.com>
2020-01-07 16:09:56 +0530

    [yaminijyothi] add. SumofThree integers programm added

---
+++ src/main/java/com/bridgelabz/Functionalprogramming/SumofThree.java
@@ -0,0 +1,42 @@
+/* 
+ * @purpose : Demonstrate the sum of three integers is zero
+ * @ file   : SumofThree.java
+ * @ author : yaminijyothi
+ */
+package com.bridgelabz.Functionalprogramming;
+
+import com.bridgelabz.utility.Utility;
+
+public class SumofThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,k,count=0;
+        System.out.println("enter how many numbers you want to enter: ");
+        int n=Utility.getint();
+        System.out.println("enter "+n+" elements:");
+        int arr[]=new int[n];
+        for(i=0;i<n;i++)
+        {
+          arr[i]=Utility.getint();	
+        }
+        //logic for sum of three integers equals to zero
+        for(i=0;i<n;i++)
+        {
+        	for(j=i+1;j<n;j++)
+        	{
+        		for(k=j+1;k<n;k++)
+        		{
+        			if(arr[i]+arr[j]+arr[k]==0)
+        			{
+        				count++;
+        				System.out.println(arr[i]+","+arr[j]+","+arr[k]);
+        			}
+        		}
+        	}
+        }
+        //prints the number of triplets gives zero
+        System.out.println(" number of triplets are:"+count);
+	}
+
+}
