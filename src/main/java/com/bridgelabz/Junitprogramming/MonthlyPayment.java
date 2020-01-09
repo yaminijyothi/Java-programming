1d726c58ca6b945235b1a95281a47f09936e187a
yaminijyothi <yamini0234@gmail.com>
2020-01-09 19:22:18 +0530

    [yaminijyothi] add. calculate the monthly payment by using commandline argument program added

---
+++ src/main/java/com/bridgelabz/Junitprogramming/MonthlyPayment.java
@@ -0,0 +1,22 @@
+/* 
+ * @purpose : Demonstrate the monthly  payments by using formula
+ * @ file   : Monthlypayment.java
+ * @ author : yaminijyothi
+ */
+package com.bridgelabz.Junitprogramming;
+
+public class MonthlyPayment {
+       //main method
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+      double p=Double.parseDouble(args[0]);
+      double  y=Double.parseDouble(args[1]);
+      double  r=Double.parseDouble(args[2]);
+      double n=12*y;
+      double R=r/(12*100);
+      double payment=p*r/(1-Math.pow(1+r,-n));
+      System.out.println("payment:"+payment);
+      
+	}
+
+}
