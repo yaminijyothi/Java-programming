2c7b71d22d3de57bfa04566a1a1bc7e7732edb6e
yaminijyothi <yamini0234@gmail.com>
2020-01-11 17:30:36 +0530

    [yaminijyothi] refactor. creating method i program

--- src/main/java/com/bridgelabz/Algorithmprograms/BubbleSort.java
+++ src/main/java/com/bridgelabz/Algorithmprograms/BubbleSort.java
@@ -23,22 +23,26 @@ import com.bridgelabz.utility.Utility;
 
 					a[i] = Utility.getint();
 				}
-				for(i=0;i<n;i++)
+				
+			sort(n,a);
+}
+			public static String sort(int n,int[] a) {
+				
+				for(int i=0;i<n;i++)
 				{
-					for(j=1;j<n-i;j++)
+					for(int j=1;j<n-i;j++)
 					{
 						if(a[j-1]>a[j])
 						{
-							temp=a[j-1];
+						int	temp=a[j-1];
 							a[j-1]=a[j];
 							a[j]=temp;
 			  
 				     	}
 			     	}
 				}
-			
-					System.out.print(Arrays.toString(a));
-
+				String rev = Arrays.toString(a);
+				return rev;
 			}
 
 		}
