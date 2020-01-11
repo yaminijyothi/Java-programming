be238bd54104a7cbade82566981e5cc104108b89
yaminijyothi <yamini0234@gmail.com>
2020-01-11 18:59:29 +0530

    [yaminijyothi] refactor. add function to program

--- src/main/java/com/bridgelabz/Junitprogramming/ToBinary.java
+++ src/main/java/com/bridgelabz/Junitprogramming/ToBinary.java
@@ -11,10 +11,22 @@ public class ToBinary {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("enter decimal number:");
-  int dec=Utility.getint();
-  String  binary=Integer.toBinaryString(dec);
-  System.out.println(binary);
+		System.out.println("enter decimal number:");
+		int n=Utility.getint();
+		System.out.println(DecToBin(n));
+	}
+	public static String DecToBin(int n)
+	{
+		if(n==0)
+			return "0";
+		String bin="";
+			while(n>0)
+			{
+				bin=((n & 1)==0?'0':'1')+bin;
+				n=n/2;
+			}
+		return bin;
 	}
 
 }
+
