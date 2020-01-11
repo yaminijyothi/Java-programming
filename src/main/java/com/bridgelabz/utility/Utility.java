e342077b0e28d93fc48be8535acbf0a544d345bb
yaminijyothi <yamini0234@gmail.com>
2020-01-11 17:39:52 +0530

    [yaminijyothi] refactor. added another function

--- src/main/java/com/bridgelabz/utility/Utility.java
+++ src/main/java/com/bridgelabz/utility/Utility.java
@@ -6,10 +6,11 @@
 package com.bridgelabz.utility;
 import java.util.Scanner;
 public class Utility {
+	private static final String String = null;
 	static Scanner sc=new Scanner(System.in);
 	
 	public static String getString() {
-		return sc.next();
+		return sc.nextLine();
 	}
 	public static int getint() {
 		return sc.nextInt();
@@ -26,5 +27,24 @@ public class Utility {
    public static boolean getBoolean() {
 	   return sc.nextBoolean();
    }
-   
+public static String sort(String string) {
+	// TODO Auto-generated method stub
+char[] ch=string.toCharArray();
+String out=" ";
+for(int i=0;i<ch.length;i++)
+{
+	for(int j=i+1;j<ch.length;j++)
+	{
+		if(ch[i]>ch[j]);
+		{
+			char temp=ch[i];
+			ch[i]=ch[j];
+			ch[j]=temp;
+		}
+	}
+	out=out+ch[i];
+	
+}
+return out;
+}
 }
