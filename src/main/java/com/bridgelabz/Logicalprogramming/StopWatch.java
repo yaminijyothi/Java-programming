507596e8a7beaaa2134492d277902c26a900ba5c
yaminijyothi <yamini0234@gmail.com>
2020-01-13 19:13:10 +0530

    [yaminijyothi] refactor. added return statement for testing

--- src/main/java/com/bridgelabz/Logicalprogramming/StopWatch.java
+++ src/main/java/com/bridgelabz/Logicalprogramming/StopWatch.java
@@ -8,21 +8,21 @@ package com.bridgelabz.Logicalprogramming;
 import com.bridgelabz.utility.Utility;
 
 public class StopWatch {
-	 long starttime=0;
-	 long endtime=0;
-	 long elapsed;
+	 static long starttime=0;
+	 static long endtime=0;
+	 static long elapsed;
 //stating time
-	public long start() {
+	public static long start(int s) {
 		starttime = System.currentTimeMillis();
 		return starttime;
 	}
 //ending time
-	public long end() {
+	public static  long end(int e) {
 		endtime = System.currentTimeMillis();
 		return endtime;
 	}
 //to calculate elapsed time by using formula endtime-starttime
-	public long elapsed() {
+	public static long elapsed() {
 		elapsed = endtime - starttime;
 	System.out.println("elapsed time:"+elapsed);
 	return elapsed;
@@ -34,10 +34,10 @@ public class StopWatch {
 		StopWatch StopWatch=new StopWatch();
      System.out.println("start time to press 0:");
      int s=Utility.getint();
-     StopWatch.start();
+     StopWatch.start(s);
      System.out.println("end time to press 1");
      int e=Utility.getint();
-     StopWatch.end();
+     StopWatch.end(1);
      long l=StopWatch.elapsed();
     //changing milliseconds  to seconds and prints
      System.out.println("In seconds:"+(l/1000)+"sec");
