ae5603d6da3ddd8078a94038f06f4b5adc0e91f3
yaminijyothi <yamini0234@gmail.com>
2020-01-13 19:19:08 +0530

    [yaminijyothi] refactor. added return statement for testing

--- src/main/java/com/bridgelabz/basicprogramming/Username.java
+++ src/main/java/com/bridgelabz/basicprogramming/Username.java
@@ -1,26 +1,40 @@
 package com.bridgelabz.basicprogramming;
 
 import com.bridgelabz.utility.Utility;
+// User Input and Replace String Template �Hello <<UserName>>, How are you?�.
 
 public class Username {
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     String data="Hi username how are you";
-     String[] space=data.split(" ");
-     String username="";
-     for(int i=0;i<space.length;i++)
-     {
-    	 if(space[i].equalsIgnoreCase("username")) {
-    	 
-    		 System.out.println("enter user name:");
-              space[i]=Utility.getString();
-     }
-     username+=space[i]+" ";
-	}
-    
-	System.out.println(username);
-	}
+    static String userName="";
+    static String data="Hello userName, How are you?";
+    public static void main(String[] args)
+    {
+        System.out.println("enter user name:");
+        userName=Utility.getString();
+        if(true);
+        {
+            stringReplacer(userName);
+        }
+        
+    }
+    public static boolean length(String userName)
+    {
+        //giving condition that userName should be grater than 3.
+        if(userName.length()<3)
+        {
+            System.out.println("userName length should be greater than 3");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    //replacing string.
+    public static String stringReplacer(String userName)
+    {    
+        System.out.println(data.replaceAll("userName", userName));
+        return userName;
+    }
 }
 
-	
+
