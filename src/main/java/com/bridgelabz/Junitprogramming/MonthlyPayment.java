82ae470867165d3db71424fbef81563cea26fd45
yaminijyothi <yamini0234@gmail.com>
2020-01-13 19:08:02 +0530

    [yaminijyothi] refactor. added return statement for testing

--- src/main/java/com/bridgelabz/Junitprogramming/MonthlyPayment.java
+++ src/main/java/com/bridgelabz/Junitprogramming/MonthlyPayment.java
@@ -12,10 +12,15 @@ public class MonthlyPayment {
       double p=Double.parseDouble(args[0]);
       double  y=Double.parseDouble(args[1]);
       double  r=Double.parseDouble(args[2]);
+      payment(p,y,r);
+	}
+	public static double payment(double p,double y,double r)
+	{
       double n=12*y;
       double R=r/(12*100);
       double payment=p*r/(1-Math.pow(1+r,-n));
       System.out.println("payment:"+payment);
+      return payment;
       
 	}
 
