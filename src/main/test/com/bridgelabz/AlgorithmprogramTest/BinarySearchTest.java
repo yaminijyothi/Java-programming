0ad162f133581ceaca578a6d118ddc41756ee3f3
yaminijyothi <yamini0234@gmail.com>
2020-01-14 11:17:34 +0530

    [yaminijyothi] add. added testcases

---
+++ src/main/test/com/bridgelabz/AlgorithmprogramTest/BinarySearchTest.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.AlgorithmprogramTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.bridgelabz.Algorithmprograms.BinarySearch;
+
+class BinarySearchTest {
+
+	@Test
+	void test() {
+		String str="my name is yamini";
+		int n=BinarySearch.stringBinarySearch();
+		assertEquals(n,BinarySearch.stringBinarySearch());
+	}
+
+}
