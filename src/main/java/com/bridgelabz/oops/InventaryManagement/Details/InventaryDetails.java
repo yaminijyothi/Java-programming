dc34692eb6cc6bab3121c0782062b70b987a58af
yaminijyothi <yamini0234@gmail.com>
2020-01-23 16:18:49 +0530

    [yaminijyothi] add. inventarydetails added.

---
+++ src/main/java/com/bridgelabz/oops/InventaryManagement/Details/InventaryDetails.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.oops.InventaryManagement.Details;
+//returns the data what we given  by us
+public class InventaryDetails {
+	private String name;
+	private long weight;
+	private long price;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getWeight() {
+		return weight;
+	}
+	public void setWeight(long weight) {
+		this.weight = weight;
+	}
+	public long getPrice() {
+		return price;
+	}
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+	return "InventoryDetails [name=" + name + ", weight=" + weight + ", price=" + price + "]";
+}
+}
+
