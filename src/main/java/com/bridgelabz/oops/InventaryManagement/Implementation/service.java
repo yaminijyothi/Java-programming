49149ccd21efb57138e0fa5c79b35b79bb6fa873
yaminijyothi <yamini0234@gmail.com>
2020-01-23 16:14:46 +0530

    [yaminijyothi] add. service added.

---
+++ src/main/java/com/bridgelabz/oops/InventaryManagement/Implementation/service.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.oops.InventaryManagement.Implementation;
+
+public interface service {
+	// implemented by ServiceImplementation
+	void add() throws Exception;
+	void display();
+}
