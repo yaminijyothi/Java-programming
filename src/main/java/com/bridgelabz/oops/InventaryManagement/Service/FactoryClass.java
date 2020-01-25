3bbf7ee878d81cecc76e67fb39fa0057f26d9d43
yaminijyothi <yamini0234@gmail.com>
2020-01-25 17:44:46 +0530

    [yaminijyothi] add. mediator is added.

--- src/main/java/com/bridgelabz/oops/InventaryManagement/Service/FactoryClass.java
+++ src/main/java/com/bridgelabz/oops/InventaryManagement/Service/FactoryClass.java
@@ -1,11 +1,10 @@
 package com.bridgelabz.oops.InventaryManagement.Service;
 
 import com.bridgelabz.oops.InventaryManagement.Implementation.ServiceImplementation;
-//like mediator between ServiceImlementation and  controller
+
 public class FactoryClass {
-	public static ServiceImplementation factoryMethod() {
+	public ServiceImplementation factoryMethod() {
 		ServiceImplementation impl=new ServiceImplementation();
 		return impl;
 	}
 }
-
