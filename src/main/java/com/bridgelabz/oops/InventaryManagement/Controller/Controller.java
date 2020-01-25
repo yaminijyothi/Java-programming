04b83db2966a3107ca694d96835747855f574815
yaminijyothi <yamini0234@gmail.com>
2020-01-25 17:48:47 +0530

    [yaminijyothi] add. main class added.

--- src/main/java/com/bridgelabz/oops/InventaryManagement/Controller/Controller.java
+++ src/main/java/com/bridgelabz/oops/InventaryManagement/Controller/Controller.java
@@ -1,34 +1,57 @@
 /*purpose : Inventarymanagement of rice,wheat,pulses using json
 file    : Controller.java*/
 package com.bridgelabz.oops.InventaryManagement.Controller;
+import com.bridgelabz.oops.InventaryManagement.Exceptions.InvalidChoice;
 import com.bridgelabz.oops.InventaryManagement.Implementation.ServiceImplementation;
 import com.bridgelabz.oops.InventaryManagement.Service.FactoryClass;
 import com.bridgelabz.oops.InventaryManagement.Utility.Utility;
 
+@SuppressWarnings("unused")
 public class Controller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		FactoryClass fc = new FactoryClass();
-		ServiceImplementation ser = fc.factoryMethod();
-		int count = 0;
+		FactoryClass srvc = new FactoryClass();
+		ServiceImplementation inventory = srvc.factoryMethod();
+		int c = 0;
 		do {
-			System.out.println("Options:"+"\n"+"1.Add items"+"\n"+"2.Display details");
+			System.out.println("1.To creat JsonFile");
+			System.out.println("2.AddItems to Product");
+			System.out.println("3.DisplayItems of Product");
+			System.out.println("4.Calculate the price");
+			System.out.println("5.Remove Item From Product");
+			System.out.println("6.To Know the Products in Inventory");
 			int ch;
 			try {
 				ch = Utility.getInt();
 				switch (ch) {
-				case 1:
-					ser.add();
-					count++;
+				case 1: 
+					inventory.createJsonFile();
 					break;
 				case 2:
-					ser.display();
-					count++;
+					inventory.addItems();
+					c++;
+					break;
+				case 3:
+					inventory.displayItems();
+					c++;
+					break;
+				case 4:
+					inventory.calculate();
+					c++;
+					break;
+				case 5:
+					inventory.removeItem();
+				    c++;
+				    break;
+				case 6:
+					inventory.inventoryProducts();
+					break;
+				
+				default:
+					//throw new InvalidChoice();
 				}
-				System.out.println("Enter 1 to continue");
-				System.out.println("Enter 0 exit");
-				System.out.println("Enter ur option:");
+				System.out.println();
+				System.out.println("1=>continue"+"\t"+"0=>Exit");
 				int ch1 = Utility.getInt();
 				switch (ch1) {
 				case 1:
@@ -37,13 +60,12 @@ public class Controller {
 				case 0:
 					System.exit(0);
 					break;
-			    default:System.out.println("enter valid option");
+			  // default:throw new InvalidChoice();
 				}
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
+				
 				e.printStackTrace();
 			}
-		} while (count >= 1);
+		} while (c >=1);
 	}
-
 }
