76348119d7d3d414a5e2d1d2bc96abf6774da60c
yaminijyothi <yamini0234@gmail.com>
2020-01-27 17:51:33 +0530

    [yaminijyothi] add. Exception handling added.

---
+++ src/main/java/com/bridgelabz/oops/StockManagement/Exception/InvalidChoice.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.oops.StockManagement.Exception;
+//exception  occurs in invalid input
+public class InvalidChoice {
+	public String toString() {
+		return "Invalid Option";
+	}
+}
