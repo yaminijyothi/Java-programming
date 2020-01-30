96d92b373dea1d56de614c1c48bb59b809d8357e
yaminijyothi <yamini0234@gmail.com>
2020-01-30 10:08:07 +0530

    [yaminijyothi] add. abstract class added.

---
+++ src/main/java/com/bridgelabz/DesignPatterns/CreationalDesignPatterns/Factory/Computer.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.DesignPatterns.CreationalDesignPatterns.Factory;
+
+public abstract class Computer {
+ public abstract String getRAM();
+ public abstract String getHDD();
+ public abstract String getCPU();
+ public String toString() {
+	 return "RAM:"+this.getRAM()+"  HDD:"+this.getHDD()+"  CPU:"+this.getCPU();
+ }
+}
