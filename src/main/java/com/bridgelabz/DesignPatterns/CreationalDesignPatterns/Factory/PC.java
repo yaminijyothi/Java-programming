2c5e4a72e6d663852980247665b319d59a8c371c
yaminijyothi <yamini0234@gmail.com>
2020-01-30 10:09:03 +0530

    [yaminijyothi] add. pc added.

---
+++ src/main/java/com/bridgelabz/DesignPatterns/CreationalDesignPatterns/Factory/PC.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.DesignPatterns.CreationalDesignPatterns.Factory;
+
+public class PC extends Computer{
+ private String ram;
+ private String hdd;
+ private String cpu;
+ public PC(String ram,String hdd,String cpu) {
+	 this.ram=ram;
+	 this.hdd=hdd;
+	 this.cpu=cpu;
+	 }
+ public String getRAM() {
+	 return this.ram;
+ }
+ public String getHDD() {
+	 return this.hdd;
+ }
+ public String getCPU() {
+	 return this.cpu;
+ }
+}
